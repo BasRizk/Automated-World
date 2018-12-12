9cc80d6fc0e6e50d02725a94d47fba0e142ed93d
mike13k <mikegeorge2012@gmail.com>
2018-12-12 20:12:37 +0100

    add comments and basic implementation

--- Automated-World/main.c
+++ Automated-World/main.c
@@ -28,23 +28,47 @@ int main(void)
 	float boundries[] = {0,0,0,0,0,0,0,0};
 	
 	if(calibration) {		// Is (Calibration) Mode or (Running) Mode?
+		
 		// wait for button press, if clicked:
+		while(1 /* !button? */) {}
+		
 		// car is moving and scanning the borders, limits should be set according to Cartesian coordinates:
+		
 			//* Phase 1: (Move down) till you hit a line with (Color Sensor), Set a limit (0,0)
+		while(1 /* !color sensor */) {
+			// move car down
+		}
+		
 			//* Once hit a line, (Move counter Clockwise) accordingly with the line being tracked.
+		while(1 /* !threshold */) {
+			// move car right
+		}
+		
 			//* Phase 2: Once hit a threshold of (y-change) while moving, Push a limit (current_X, 0).
-			boundries[corner_1_x] = current_x_pos;
-			boundries[corner_1_y] = 0;
+		boundries[corner_1_x] = current_x_pos;
+		boundries[corner_1_y] = 0;
+		while(1 /* !threshold */) {
+			// move car up
+		}
+		
 			//* Phase 3: Once hit a threshold of (x-change) while moving, Push a limit (past_limit_X, current_Y)
-			boundries[corner_2_x] = past_x_pos;
-			boundries[corner_2_y] = current_y_pos;
+		boundries[corner_2_x] = past_x_pos;
+		boundries[corner_2_y] = current_y_pos;
+		while(1 /* !threshold */) {
+			// move car left
+		}
+		
 			//* Phase 4: Once hit a threshold of (y-change) while moving, Push a limit (current_X, past_limit_Y)
-			boundries[corner_3_x] = current_x_pos;
-			boundries[corner_3_y] = past_y_pos;
+		boundries[corner_3_x] = current_x_pos;
+		boundries[corner_3_y] = past_y_pos;	
+		
 			//* Phase 5: Push a limit (last_X, 0)
-			boundries[corner_4_x] = past_x_pos;
-			boundries[corner_4_y] = 0;
+		boundries[corner_4_x] = past_x_pos;
+		boundries[corner_4_y] = 0;
+			
 			//* Phase 6: Rotate car and move anywhere inside the closed rectangle.
+
+
 			
 		calibration = 0;		// After calibration -> turn to (Running) Mode.
 	}
