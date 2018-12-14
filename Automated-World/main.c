1e5964cdb5460cde08188ffbd22a44e2ef60072d
mike13k <mikegeorge2012@gmail.com>
2018-12-14 18:16:26 +0100

    implement logic of calculating car position when moving

--- Automated-World/main.c
+++ Automated-World/main.c
@@ -52,6 +52,8 @@ float Xg=0,Yg=0,Zg=0;
 char buffer[20], float_[10];
 float gyro_x_calc=0, gyro_y_calc=0, gyro_z_calc=0;
 
+float unit_step = 1;
+
 enum MOTOR {A, B};
 enum DIRECTION {BACKWARD, FORWARD};
 
@@ -64,6 +66,9 @@ void MPU_Perform_Calc();
 void MPU_Read_RawValue();
 void input_key_logic();
 void move(enum MOTOR, enum DIRECTION, unsigned char speed);
+
+float get_X(float angle);
+float get_Y(float angle);
 
 
 int main(void)
@@ -79,9 +84,6 @@ int main(void)
 	stdout = &uart_output;
 	stdin  = &uart_input;
 	
-
-
-
 	while(1) {		
 		
 		// GYROSCOPE LOGIC
@@ -253,76 +255,96 @@ void Init_PWM_Config() {
 	TCCR0B = 0b00000001;
 }
 
+float get_X(float angle)
+{
+	return (unit_step * cos(angle));
+}
+
+float get_Y(float angle)
+{
+	return (unit_step * sin(angle));
+}
+
 void main_logic_commented() {
 		
-	//bool calibration = true;
-	//float current_x_pos = 0;
-	//float current_y_pos = 0;
-	//float past_x_pos = 0;
-	//float past_y_pos = 0;
-	//float boundries[] = {0,0,0,0,0,0,0,0};
-	//
-	//if(calibration) {		// Is (Calibration) Mode or (Running) Mode?
-		//
-		//// wait for button press, if clicked:
-		//while(1 /* !button? */) {}
-		//
-		//// car is moving and scanning the borders, limits should be set according to Cartesian coordinates:
-		//
-			////* Phase 1: (Move down) till you hit a line with (Color Sensor), Set a limit (0,0)
-		//while(1 /* !color sensor */) {
-			//// move car down
-		//}
-		//
-			////* Once hit a line, (Move counter Clockwise) accordingly with the line being tracked.
-		//while(1 /* !threshold */) {
-			//// move car right
-		//}
-		//
-			////* Phase 2: Once hit a threshold of (y-change) while moving, Push a limit (current_X, 0).
-		//boundries[corner_1_x] = current_x_pos;
-		//boundries[corner_1_y] = 0;
-		//while(1 /* !threshold */) {
-			//// move car up
-		//}
-		//
-			////* Phase 3: Once hit a threshold of (x-change) while moving, Push a limit (past_limit_X, current_Y)
-		//boundries[corner_2_x] = past_x_pos;
-		//boundries[corner_2_y] = current_y_pos;
-		//while(1 /* !threshold */) {
-			//// move car left
-		//}
-		//
-			////* Phase 4: Once hit a threshold of (y-change) while moving, Push a limit (current_X, past_limit_Y)
-		//boundries[corner_3_x] = current_x_pos;
-		//boundries[corner_3_y] = past_y_pos;	
-		//
-			////* Phase 5: Push a limit (last_X, 0)
-		//boundries[corner_4_x] = past_x_pos;
-		//boundries[corner_4_y] = 0;
-			//
-			////* Phase 6: Rotate car and move anywhere inside the closed rectangle.
-//
-//
-			//
-		//calibration = 0;		// After calibration -> turn to (Running) Mode.
-	//}
-	//else if(!calibration) {	// else if (Running) Mode:
-		//// wait for button press, if clicked:
-			//// Calculate (toBeTraveledDistance) relative to the PWM to be generated
-			//// Calculate (PositionToBeAt) using (CurrentPosition + toBeTraveledDistance)
-			//
-			//if (1) {		// Is PositionToBeAt in &bounds or Not:
-				//// Calculate &bounds using (limits[4] array] -- Could be done previously
-		//
-				//if (1) {		// if PositionToBeAt in &bounds:
-					//// Generate PWM accordingly and move car.
-				//}
-				//else {
-					//// Stop Car and flash lights accordingly
-				//}
-			//}
-	//}
-	//
-	//return 0;
+	bool calibration = true;
+	float current_x_pos = 0;
+	float current_y_pos = 0;
+	float past_x_pos = 0;
+	float past_y_pos = 0;
+	float boundries[] = {0,0,0,0,0,0,0,0};
+		
+	float toBeTraveled_X = 0;
+	float toBeTraveled_Y = 0;
+	float toBePosition_X = 0;
+	float toBePosition_Y = 0;
+	
+	if(calibration) {		// Is (Calibration) Mode or (Running) Mode?
+		
+		// wait for button press, if clicked:
+		while(1 /* !button? */) {}
+		
+		// car is moving and scanning the borders, limits should be set according to Cartesian coordinates:
+		
+			//* Phase 1: (Move down) till you hit a line with (Color Sensor), Set a limit (0,0)
+		while(1 /* !color sensor */) {
+			// move car down
+		}
+		
+			//* Once hit a line, (Move counter Clockwise) accordingly with the line being tracked.
+		while(1 /* !threshold */) {
+			// move car right
+		}
+		
+			//* Phase 2: Once hit a threshold of (y-change) while moving, Push a limit (current_X, 0).
+		boundries[corner_1_x] = current_x_pos;
+		boundries[corner_1_y] = 0;
+		while(1 /* !threshold */) {
+			// move car up
+		}
+		
+			//* Phase 3: Once hit a threshold of (x-change) while moving, Push a limit (past_limit_X, current_Y)
+		boundries[corner_2_x] = past_x_pos;
+		boundries[corner_2_y] = current_y_pos;
+		while(1 /* !threshold */) {
+			// move car left
+		}
+		
+			//* Phase 4: Once hit a threshold of (y-change) while moving, Push a limit (current_X, past_limit_Y)
+		boundries[corner_3_x] = current_x_pos;
+		boundries[corner_3_y] = past_y_pos;	
+		
+			//* Phase 5: Push a limit (last_X, 0)
+		boundries[corner_4_x] = past_x_pos;
+		boundries[corner_4_y] = 0;
+			
+			//* Phase 6: Rotate car and move anywhere inside the closed rectangle.
+
+
+			
+		calibration = 0;		// After calibration -> turn to (Running) Mode.
+	}
+	else if(!calibration) {	// else if (Running) Mode:
+		// wait for button press, if clicked:
+			
+			// Calculate (toBeTraveledDistance) relative to the PWM to be generated
+			toBeTraveled_X = get_X(gyro_x_calc);
+			toBeTraveled_Y = get_Y(gyro_x_calc);
+			
+			// Calculate (PositionToBeAt) using (CurrentPosition + toBeTraveledDistance)
+			toBePosition_X = current_x_pos + toBeTraveled_X;
+			toBePosition_Y = current_y_pos + toBeTraveled_Y;
+			
+			if (toBePosition_X < boundries[corner_1_x] && toBePosition_X > boundries[corner_4_x] && toBePosition_Y > 0 && toBePosition_Y < boundries[corner_2_y]) {		// Is PositionToBeAt in &bounds or Not:
+				
+				// if PositionToBeAt in &bounds:
+				// Generate PWM accordingly and move car.
+					
+			}
+			else {
+				// Stop Car and flash lights accordingly
+			}
+			
+	}
+	
 }
